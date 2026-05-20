a5610503f6ac0b5922136a3c974bf0120e457971
Nemanja <npolic2@gmail.com>
2026-05-21 00:41:16 +0200

    initial commit with some migrations

---
+++ src/config/cas.php
@@ -0,0 +1,8 @@
+<?php
+
+return [
+    'api_key' => env('CAS_API_KEY'),
+    'executable_path' => env('CAS_EXECUTABLE_PATH', '/usr/bin/octave'),
+    'slowdown_ms' => (int) env('CAS_SLOWDOWN_MS', 300),
+    'stats_interval_minutes' => (int) env('STATS_INTERVAL_MINUTES', 10),
+];
